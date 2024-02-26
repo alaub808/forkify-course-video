ab9d9b6d35a5780250e49b176617a90d27c96f02
alaub808 <avery.laubach808@gmail.com>
2024-02-26 14:10:56 -0500

    Untrack files in .gitignore

---
+++ starter/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'd1d2306b-7fbc-4bcf-b62b-eda999863a3e';
+export const MODAL_CLOSE_SEC = 2.5;
